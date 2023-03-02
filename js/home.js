21a7c89ead4b4ebaf293b3f06a168895a5279af1
Nami Miyashiro <nami.miyashiro@gmail.com>
2023-03-02 02:40:57 -0800

    add deposit function

--- js/home.js
+++ js/home.js
@@ -27,7 +27,33 @@ window.onload = function() {
     depositModal.classList.remove("active");
     overlay.classList.remove("active");
   });
-};
+
+  //  ----------------- Deposit Function -----------------  //
+  showDeposit();
+
+  // Save deposit data
+  document.getElementById("deposit").onclick = function saveDeposit() {
+    const userInput = document.getElementById("inputDeposit").value;
+    console.log(`userInput = ${userInput}`);
+    localStorage.setItem(`userInput`, userInput);
+    showDeposit();
+  }
+
+  //Get deposit data
+  // const depositStorage = localStorage.getItem(`userInput`);
+  // console.log(`depositStorage = ${depositStorage}`);
+
+  //Show deposit data
+  function showDeposit() {
+    const depositStorage = localStorage.getItem(`userInput`);
+    console.log(`depositStorage = ${depositStorage}`);
+    if (depositStorage) {
+      document.getElementsByClassName("moneySavedPrice")[0].textContent = depositStorage;
+    } else {
+      document.getElementsByClassName("moneySavedPrice")[0].innerHTML = "No Data";
+    }
+  }
+} 
 
 // parameter: userAmount object (userInput, userOwn), main currency
 // convert all values in userAmount object to user's main currency based on inputted currentRateList(Object)
@@ -35,6 +61,14 @@ window.onload = function() {
 // function calculateAmount(userAmount, mainCurrency, currentRateList) {
 
 // }
+//  ----------------- Deposit Function -----------------  //
+// document.getElementById("deposit").onclick = function saveDeposit() {
+//   const depositData = document.getElementById("inputDeposit").value;
+//   console.log(`depositData = ${depositData}`);
+//   localStorage.setItem(`depositData`, depositData);
+// }
+
+
 
 // parameter: userObject
 // calculate sum value of user input (localStorage) and user own (localStorage) in main currency
